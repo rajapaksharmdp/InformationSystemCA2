43dd093c060012508c3a9d0587103d13fa763004
rajapaksharmdp <37535230+rajapaksharmdp@users.noreply.github.com>
2023-07-07 12:08:45 +0100

    Delete subjects

--- main.py
+++ main.py
@@ -38,6 +38,11 @@ def create_subject():
     # Load existing subjects
     subjects_data = load_subjects_from_file()
 
+    # Check if subject ID already exists
+    existing_ids = [subject['subject_id'] for subject in subjects_data]
+    if new_subject['subject_id'] in existing_ids:
+        return jsonify({'error': 'Subject ID already exists'}), 400
+
     # Assign a new subject ID
     new_subject_id = len(subjects_data) + 1
     new_subject['subject_id'] = new_subject_id
@@ -49,6 +54,9 @@ def create_subject():
     save_subjects_to_file(subjects_data)
 
     return jsonify(new_subject), 201  # Return the created subject with 201 status code
+
+
+
 
 # update subject
 @app.route('/modify_subject/<int:subject_id>', methods=['PUT'])
@@ -70,6 +78,26 @@ def update_subject(subject_id):
     save_subjects_to_file(subjects_data)
 
     return jsonify(updated_subject), 200  # Return the updated subject with 200 status code
+
+
+# delete subject
+@app.route('/delete_subject/<int:subject_id>', methods=['DELETE'])
+def delete_subject(subject_id):
+    # Load existing subjects
+    subjects_data = load_subjects_from_file()
+
+    # Find the subject to delete by subject_id
+    for subject in subjects_data:
+        if subject['subject_id'] == subject_id:
+            # Remove the subject from the list
+            subjects_data.remove(subject)
+            break
+
+    # Save the updated subjects data to the file
+    save_subjects_to_file(subjects_data)
+
+    return jsonify({'message': 'Subject deleted'}), 200  # Return a response with a message and 200 status code
+
 
 # Run the Flask app on localhost with port 8080
 if __name__ == '__main__':
