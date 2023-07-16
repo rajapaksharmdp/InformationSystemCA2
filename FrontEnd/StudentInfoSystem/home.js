9513ad0a48491a9043594cf54ca02ea7c6dc4fdc
rajapaksharmdp <37535230+rajapaksharmdp@users.noreply.github.com>
2023-07-16 12:59:54 +0100

    role cotrol

--- FrontEnd/StudentInfoSystem/home.js
+++ FrontEnd/StudentInfoSystem/home.js
@@ -1,33 +1,39 @@
 document.getElementById('loginBtn').addEventListener('click', () => {
-    let username = document.getElementById('username_Input').value;
-    let password = document.getElementById('password_Input').value;
-  
-    let loginData = {
-      username: username,
-      password: password,
-    };
-  
-    fetch('http://localhost:8080/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(loginData),
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        if (data.message === 'Login successful') {
-          console.log("hari");
-          window.location.href = 'subjects.html'; // Redirect to subjects.html on successful login
+  let username = document.getElementById('username_Input').value;
+  let password = document.getElementById('password_Input').value;
+
+  let loginData = {
+    username: username,
+    password: password,
+  };
+
+  fetch('http://localhost:8080/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(loginData),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      if (data.message === 'Login successful') {
+        console.log("hari");
+        // Check the user's role and redirect accordingly
+        if (data.role === 'admin') {
+          window.location.href = 'subjects.html';
+        } else if (data.role === 'teacher') {
+          window.location.href = 'grades.html'; 
         } else {
-          console.log('Login failed:', data.message);
-          // Handle the failed login case here
+          console.log('Unknown role:', data.role);
         }
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
+      } else {
+        console.log('Login failed:', data.message);
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
 });
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -75,6 +81,7 @@ document.addEventListener('DOMContentLoaded', () => {
             successMessage.textContent = 'Registration successful! You can now log in.';
             let modalBody = document.querySelector('#registrationModal .modal-body');
             modalBody.appendChild(successMessage);
+
         setTimeout(() => {
           let registrationModal = new bootstrap.Modal(document.getElementById('registrationModal'));
           registrationModal.hide();
