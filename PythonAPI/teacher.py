4aefd74ecb647caf21f228ee3bf5f7647caeb59d
Nandhu2514 <134452080+Nandhu2514@users.noreply.github.com>
2023-07-17 12:06:45 +0100

    teachers_data

--- PythonAPI/teacher.py
+++ PythonAPI/teacher.py
@@ -1,3 +1,7 @@
+import json
+from flask import Flask, json, request
+from flask_cors import CROS
+
 # File path to the teachers data
 teachers_file = 'Storage/techers.json'
 
@@ -7,4 +11,9 @@ def load_teachers_from_file():
         teachers_dat = json.load(f)
         return teachers_data
 
+# Save teachers to the file
+def save_teachers_to_the_file(teachers_data):
+    with open(techers_file,'w') as f:
+        json.dump(techers_data, f)
+        
 
