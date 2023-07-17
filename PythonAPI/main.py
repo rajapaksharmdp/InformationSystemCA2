d40d0d6215c6d52d0b3c89dc80ed8fad309e6bdd
rajapaksharmdp <37535230+rajapaksharmdp@users.noreply.github.com>
2023-07-17 21:32:42 +0100

    Update main.py

--- PythonAPI/main.py
+++ PythonAPI/main.py
@@ -1,4 +1,3 @@
-
 import json
 from flask import Flask, jsonify, request
 from flask_cors import CORS
@@ -194,7 +193,7 @@ def delete_subject(subject_id):
 @app.route('/filter_subjects', methods=['GET'])
 def filter_subjects():
     subjects_data = load_subjects_from_file()
-    
+
     # Get the filter parameters from the query string
     subject_id = request.args.get('subject_id')
     subject_name = request.args.get('subject_name')
@@ -225,6 +224,70 @@ def filter_subjects():
         filtered_subjects = sort_subjects(filtered_subjects, sort_by, sort_order)
 
     return jsonify(filtered_subjects)
+
+
+##############################################################################################################################
+# teachers Section
+teachers_file = 'Storage/teachers.json'
+
+
+def load_teachers_from_file():
+    with open(teachers_file) as f:
+        teachers_data = json.load(f)
+    return teachers_data
+
+
+def save_teachers_to_file(teachers_data):
+    with open(teachers_file, 'w') as f:
+        json.dump(teachers_data, f, indent=4)
+
+
+@app.route('/getteachers', methods=['GET'])
+def get_teachers():
+    teachers_data = load_teachers_from_file()
+    return jsonify(teachers_data)
+
+
+@app.route('/addteachers', methods=['POST'])
+def create_teacher():
+    new_teacher = request.json
+    if not new_teacher or not all(key in new_teacher for key in ['teacher_id', 'name', 'email', 'phone_no', 'address']):
+        return jsonify(
+            {"message": "Invalid teacher data. Required fields: teacher_id, name, email, phone_no, address"}), 400
+
+    teachers_data = load_teachers_from_file()
+    teachers_data.append(new_teacher)
+    save_teachers_to_file(teachers_data)
+    return jsonify(new_teacher), 201
+
+
+@app.route('/update_teachers/<int:teacher_id>', methods=['PUT'])
+def update_teacher(teacher_id):
+    updated_teacher = request.json
+    teachers_data = load_teachers_from_file()
+
+    for i, teacher in enumerate(teachers_data):
+        if teacher['teacher_id'] == teacher_id:
+            teachers_data[i].update(updated_teacher)
+            save_teachers_to_file(teachers_data)
+            return jsonify(updated_teacher), 200
+
+    return jsonify({"message": "Teacher not found"}), 404
+
+
+@app.route('/delete_teachers/<int:teacher_id>', methods=['DELETE'])
+def delete_teacher(teacher_id):
+    teachers_data = load_teachers_from_file()
+    for i, teacher in enumerate(teachers_data):
+        if teacher['teacher_id'] == teacher_id:
+            del teachers_data[i]
+            save_teachers_to_file(teachers_data)
+            return jsonify({"message": "Teacher deleted"}), 200
+
+    return jsonify({"message": "Teacher not found"}), 404
+
+
+#############################################################################################################################
 
 
 # Run the Flask app on localhost with port 8080
