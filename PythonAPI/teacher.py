3738fc0602f78d3a71fd02ed1d37e2d1620730ab
Nandhu2514 <134452080+Nandhu2514@users.noreply.github.com>
2023-07-17 13:13:13 +0100

    teachers.py update

--- PythonAPI/teacher.py
+++ PythonAPI/teacher.py
@@ -27,7 +27,7 @@ def get_teachers():
     teachers_data = load_teachers_from_file()
     return jsonify(teachers_data)
 
-@app.route('/teachers', methods=['POST'])
+@app.route('/addteachers', methods=['POST'])
 def create_teacher():
     new_teacher = request.json
     if not new_teacher or not all(key in new_teacher for key in ['teacher_id', 'name', 'email', 'phone_no', 'address']):
@@ -38,7 +38,7 @@ def create_teacher():
     save_teachers_to_file(teachers_data)
     return jsonify(new_teacher), 201
 
-@app.route('/teachers/<int:teacher_id>', methods=['PUT'])
+@app.route('/updateteachers/<int:teacher_id>', methods=['PUT'])
 def update_teacher(teacher_id):
     updated_teacher = request.json
     if not updated_teacher or not all(key in updated_teacher for key in ['name', 'email', 'phone_no', 'address']):
@@ -53,7 +53,7 @@ def update_teacher(teacher_id):
 
     return jsonify({"message": "Teacher not found"}), 404
 
-@app.route('/teachers/<int:teacher_id>', methods=['DELETE'])
+@app.route('/deleteteachers/<int:teacher_id>', methods=['DELETE'])
 def delete_teacher(teacher_id):
     teachers_data = load_teachers_from_file()
     for i, teacher in enumerate(teachers_data):
