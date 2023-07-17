09d2a1b67b73a499933812621ab0ff4f3711c6ca
Nandhu2514 <134452080+Nandhu2514@users.noreply.github.com>
2023-07-17 12:40:29 +0100

    teachers.py update

--- PythonAPI/teacher.py
+++ PythonAPI/teacher.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, json, request
+from flask import Flask, jsonify, request
 from flask_cors import CROS
 
 # File path to the teachers data
@@ -15,5 +15,23 @@ def load_teachers_from_file():
 def save_teachers_to_the_file(teachers_data):
     with open(techers_file,'w') as f:
         json.dump(techers_data, f)
-        
+
+# get all teachers
+@app.route('/teachers', methods=['GET'])
+def get_teachers():
+   teachers_data = load_teachers_from_file()
+   return jsonify(techers_data)
+
+#create new teacher
+
+@app.route('/teachers', methods=[POST])
+def create_teacher():
+    new_teacher = request.json
+    teachers_data = load_teachers_from_file()
+    teachers_data.append(new_teacher)
+    save_teachers_to_the_file(teachers_data)
+    return jsonify(new_teacher), 201
+
+
+
 
