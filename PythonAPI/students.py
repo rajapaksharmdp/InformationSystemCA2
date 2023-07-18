24c1510c670f935e38ed720323d186ac3bfe8ed9
justi-07 <137523682+justi-07@users.noreply.github.com>
2023-07-19 00:48:25 +0100

    Update students.py
    fix error

--- PythonAPI/students.py
+++ PythonAPI/students.py
@@ -11,51 +11,21 @@ CORS(app)
 students_file = 'Storage/students.json'
 
 
-class StudentAPI:
-    @staticmethod
-    def open_student():
-        with open(students_file) as f:
-            students_data = json.load(f)
-        return students_data
+def open_student():
+    with open(students_file) as f:
+        students_data = json.load(f)
+    return students_data
 
-    @staticmethod
-    def save_student(students_data):
-        with open(students_file, 'w') as f:
-            json.dump(students_data, f)
+def save_student(students_data):
+    with open(students_file, 'w') as f:
+        json.dump(students_data, f)
 
-    @app.route('/students', methods=['GET'])
-    def get_students(self):
-        students_data = StudentAPI.open_student()
-        return jsonify(students_data)
-
-    # new student
-
-    @app.route('/newstudent', methods=['POST'])
-    def create_student(self):
-        new_student = request.json
-
-        students_data = StudentAPI.open_student()
-        students_data.append(new_student)
-
-        StudentAPI.save_student(students_data)
-        return jsonify(new_student), 201
+@app.route('/students', methods=['GET'])
+def get_students():
+    students_data = open_student()
+    return jsonify(students_data)
 
 
-    # delete student,
-
-    @app.route('/deletestudents/<int:student_id>', methods=['DELETE'])
-    def delete_student(self, student_id):
-        students_data = StudentAPI.open_student()
-        for student in students_data:
-            if student['student_id'] == student_id:
-                students_data.remove(student)
-                break
-
-        StudentAPI.save_student(students_data)
-        return jsonify({'message': 'Student Deleted'}), 200
-
-
-student_api = StudentAPI()
 
 if __name__ == '__main__':
     app.run(port=8080)
